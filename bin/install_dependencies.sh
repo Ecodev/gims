d3b2658697876e4817b1a3eaeb503bcf3a94ba9a
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-02-19 10:44:50 +0900

    Silent install for Travis

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-# Install PostGIS
-sudo add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+echo "Installing PostGIS..."
+sudo add-apt-repository --yes ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get -qq update
 sudo apt-get -qq install postgis postgresql-9.1-postgis rubygems
 
-# Install Compass
-sudo gem install --no-rdoc --no-ri sass compass oily_png bootstrap-sass
+echo "Installing Compass..."
+sudo gem install --quiet --no-rdoc --no-ri sass compass oily_png bootstrap-sass
 
 
-if [[ "$1" = "testing" ]]; then
-    # Init database
+if [[ "$1" = "travis" ]]; then
+    echo "Init database..."
     cp config/autoload/local.php.dist config/autoload/local.php
     psql -c 'create database gims;' -U postgres
 else
-    sudo apt-get install libapache2-mod-php5 php5-pgsql php5-cli php5-gd php5-mcrypt
+    sudo apt-get install -qq libapache2-mod-php5 php5-pgsql php5-cli php5-gd php5-mcrypt
 fi
 
-# Install all PHP dependencies via composer
-./composer.phar install --dev
+echo "Installing all PHP dependencies via composer..."
+./composer.phar install --dev --quiet
