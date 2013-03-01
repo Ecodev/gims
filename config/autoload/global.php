9a6a7f7162d8c9bec4f2d5ed14febb8ad6a4b38c
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-03-01 20:19:51 +0900

    Remove MySql specific configuration

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,9 +14,6 @@
 return array(
     'db' => array(
         'driver' => 'Pdo',
-        'driver_options' => array(
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-        ),
     ),
     'service_manager' => array(
         'factories' => array(
