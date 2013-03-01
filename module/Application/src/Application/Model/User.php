3deac417a19898213c92e68206ec0de74164f99a
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-03-01 15:45:51 +0900

    Introduce ZfcUserDoctrineORM
    User creation and login is working, but the GUI is lacking

--- module/Application/src/Application/Model/User.php
+++ module/Application/src/Application/Model/User.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="`user`")
  * @ORM\Entity(repositoryClass="Application\Repository\UserRepository")
  */
-class User extends AbstractModel
+class User extends AbstractModel implements \ZfcUser\Entity\UserInterface
 {
 
     /**
@@ -131,6 +131,52 @@ class User extends AbstractModel
     public function getState()
     {
         return $this->state;
+    }
+
+    /**
+     * Forward method call, because we only have a name
+     * @return string
+     */
+    public function getDisplayName()
+    {
+        return $this->getUsername();
+    }
+
+    /**
+     * Forward method call, because we only have a name
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->getName();
+    }
+
+    /**
+     * Forward method call, because we only have a name
+     * @return string
+     */
+    public function setDisplayName($displayName)
+    {
+        return $this->setUsername($displayName);
+    }
+
+    /**
+     * Not implemented
+     * @throws \Exception
+     */
+    public function setId($id)
+    {
+        // @TODO: Is it really necessary to set the ID ?
+        throw new Exception("Not implemented, because Doctrine will set the ID automagically");
+    }
+
+    /**
+     * Forward method call, because we only have a name
+     * @return string
+     */
+    public function setUsername($username)
+    {
+        return $this->setName($username);
     }
 
 }
