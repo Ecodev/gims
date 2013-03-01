7c5ad0782dcf3538d823b79c6652339d9a2ea0af
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-03-01 15:18:19 +0900

    Use variables for DB connection

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,6 @@
 return array(
     'db' => array(
         'driver' => 'Pdo',
-        'dsn' => 'pgsql:dbname=gims;host=localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
