eea670377b01dd58b081661bc9e279dedc8f4d74
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-03-19 13:17:29 +0900

    Actually use latest nodejs package, which also include npm

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -3,9 +3,9 @@
 echo "Installing PostGIS and other packages..."
 sudo apt-get install -qq software-properties-common # to get next command: add-apt-repository
 sudo add-apt-repository --yes ppa:ubuntugis/ubuntugis-unstable
-sudo add-apt-repository --yes ppa:chris-lea/node.js-legacy
+sudo add-apt-repository --yes ppa:chris-lea/node.js
 sudo apt-get -qq update
-sudo apt-get -qq install postgis postgresql-9.1-postgis rubygems npm apache2 php5-pgsql php5-cli php5-gd php5-mcrypt php5-intl
+sudo apt-get -qq install postgis postgresql-9.1-postgis rubygems nodejs apache2 php5-pgsql php5-cli php5-gd php5-mcrypt php5-intl
 
 echo "Installing Compass..."
 gem install --quiet --no-rdoc --no-ri sass compass oily_png bootstrap-sass
