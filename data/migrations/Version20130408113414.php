e25c60c46c24b8c28f40072761fe0d5d3de59dcc
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-04-08 12:06:02 +0900

    Missing database migration

---
+++ data/migrations/Version20130408113414.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20130408113414 extends AbstractMigration
+{
+
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "postgresql", "Migration can only be executed safely on 'postgresql'.");
+
+        $this->addSql("CREATE UNIQUE INDEX category_unique ON category (name, parent_id)");
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->throwIrreversibleMigrationException();
+    }
+
+}
