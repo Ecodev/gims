983c72e8e9bf4167c0ffa8f5465ae8f95277d047
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2013-06-10 18:01:55 +0200

    ability to disable questionnaires by clicking on scatter plots (WiP, no spline refresh yet)

--- module/Api/src/Api/Controller/ChartController.php
+++ module/Api/src/Api/Controller/ChartController.php
@@ -12,6 +12,13 @@ class ChartController extends \Application\Controller\AbstractAngularActionContr
         $country = $this->getEntityManager()->getRepository('Application\Model\Country')->findOneById($this->params()->fromQuery('country'));
         $filterSet = $this->getEntityManager()->getRepository('Application\Model\FilterSet')->findOneById($this->params()->fromQuery('filterSet'));
         $part = $this->getEntityManager()->getRepository('Application\Model\Part')->findOneById($this->params()->fromQuery('part'));
+        $excludeStr = $this->params()->fromQuery('exclude');
+        $refresh = $this->params()->fromQuery('refresh');
+        $excluded = array();
+        if ($excludeStr && strlen($excludeStr))
+        {
+            $excluded = split(',', $excludeStr);
+        }
 
         $questionnaires = $this->getEntityManager()->getRepository('Application\Model\Questionnaire')->findBy(array('geoname' => $country ? $country->getGeoname() : -1));
 
@@ -37,22 +44,32 @@ class ChartController extends \Application\Controller\AbstractAngularActionContr
 
             // Then add scatter points which are each questionnaire values
             foreach ($filterSet->getFilters() as $filter) {
+                $idFilter = $filter->getId();
                 $data = $calculator->computeFilterForAllQuestionnaires($filter, $questionnaires, $part);
-
                 $scatter = array(
                     'type' => 'scatter',
                     'name' => $filter->getName(),
+                    'allowPointSelect' => true,
                     'data' => array(),
                 );
                 $i = 0;
                 foreach ($data['values%'] as $surveyCode => $value) {
 
                     if (!is_null($value)) {
-                        $scatter['data'][] = array(
+                        $isExcluded = in_array($idFilter.':'.$surveyCode, $excluded);
+                        $scatterData = array(
                             'name' => $surveyCode,
+                            'id' => $idFilter.':'.$surveyCode,
                             'x' => $data['years'][$i],
                             'y' => round($value * 100, 1),
                         );
+                        // grey the ignored questionnaires scatter plots
+                        if (in_array($idFilter.':'.$surveyCode, $excluded))
+                        {
+                            $scatterData['dataLabels'] = array('color' => '#BBB');
+                            $scatterData['marker'] = array('fillColor' => '#BBB');
+                        }
+                        $scatter['data'][] = $scatterData;
                     }
                     $i++;
                 }
@@ -65,6 +82,7 @@ class ChartController extends \Application\Controller\AbstractAngularActionContr
         $chart = array(
             'chart' => array(
                 'height' => 600,
+                'animation' => $refresh ? false : true,
             ),
             // Here we use default highchart colors and symbols, but truncated at the same number of series,
             // so it will get repeated for lines and scatter points
@@ -113,6 +131,14 @@ class ChartController extends \Application\Controller\AbstractAngularActionContr
                     'dataLabels' => array(
                         'enabled' => true,
                         'format' => '{point.name}',
+                    ),
+                    'marker' => array(
+                        'states' => array(
+                            'select' => array(
+                                'fillColor' => '#BBB',
+                                'lineColor' => '#BBB',
+                            ),
+                        ),
                     ),
                 ),
             ),
