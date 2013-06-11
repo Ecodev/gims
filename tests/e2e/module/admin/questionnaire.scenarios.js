83cb9988308594b1f1150634aa56adaeedd02346
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-06-11 18:36:40 +0200

    Fix e2e tests

--- tests/e2e/module/admin/questionnaire.scenarios.js
+++ tests/e2e/module/admin/questionnaire.scenarios.js
@@ -9,7 +9,7 @@ describe('admin/questionnaire/new', function () {
     });
 
     it('should be displayed tabs', function () {
-        var panes = new Array('General info', 'Users');
+        var panes = new Array('General info');
         for (var index = 0; index < panes.length; index++) {
             var paneText = panes[index];
             expect(element('[ng-view] .nav-tabs li:eq(' + index + ')').text())
