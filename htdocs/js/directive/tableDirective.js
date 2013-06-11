c3c76728687d422271d6522712cf274532931c90
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-06-11 21:06:59 +0900

    Fix minor typo

--- htdocs/js/directive/tableDirective.js
+++ htdocs/js/directive/tableDirective.js
@@ -30,7 +30,7 @@ angular.module('myApp.directives').directive('gimsTable', function () {
         '       </div>'+
         '       </div>'+
         '       </form>'+
-        '       <div class="alert alert-info" ng-hide="table"><i class="icon-info-sign"></i>Select parameters to show table here.</div>'+
+        '       <div class="alert alert-info" ng-hide="table"><i class="icon-info-sign"></i> Select parameters to show table here.</div>'+
         '       <div ng-grid="gridOptions" class="gridStyle"></div>'+
         '       </div>',
         // The linking function will add behavior to the template
@@ -42,8 +42,8 @@ angular.module('myApp.directives').directive('gimsTable', function () {
         // Configure select2 via our helper service
             Select2Configurator.configure($scope, 'questionnaire');
             Select2Configurator.configure($scope, 'filterSet');
-        
-        
+
+
             // Configure ng-grid.
             $scope.gridOptions = {
                 data: 'table',
@@ -55,30 +55,30 @@ angular.module('myApp.directives').directive('gimsTable', function () {
                     {sortable: false, field: 'values.Total', displayName: 'Total', cellFilter: 'percent'}
                 ]
             };
-        
+
             var originalTable;
             $scope.refresh = function() {
-        
+
                 var result = [];
                 angular.forEach(originalTable, function(e) {
                     if (!$scope.showOnlyTopLevel || !e.filter.level) {
                         result.push(e);
                     }
                 });
-        
+
                 $scope.table = result;
             };
-        
+
             // Whenever one of the parameter is changed
             var uniqueAjaxRequest;
             $scope.$watch('select2.questionnaire.selected.id + select2.filterSet.selected.id', function(a) {
-        
+
                 // If they are all available ...
                 if ($scope.select2.questionnaire.selected && $scope.select2.filterSet.selected) {
                     $scope.isLoading = true;
                     $timeout.cancel(uniqueAjaxRequest);
                     uniqueAjaxRequest = $timeout(function() {
-        
+
                         // ... then, get table data via Ajax, but only once per 200 milliseconds
                         // (this avoid sending several request on page loading)
                         $http.get('/api/table',
