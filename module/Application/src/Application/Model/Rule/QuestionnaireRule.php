dddd8fa6aa7dc4303101e0bec0ce14f5b7d7e63f
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-07-31 14:20:02 +0900

    Add new QuestionnaireRule model

---
+++ module/Application/src/Application/Model/Rule/QuestionnaireRule.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Application\Model\Rule;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * QuestionnaireRule allows us to "apply" a rule to a questionnaire-part pair.
+ *
+ * @ORM\Entity(repositoryClass="Application\Repository\QuestionnaireRuleRepository")
+ * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(name="questionnaire_rule_unique",columns={"questionnaire_id", "part_id", "rule_id"})})
+ */
+class QuestionnaireRule extends \Application\Model\AbstractModel
+{
+
+    /**
+     * @var Questionnaire
+     *
+     * @ORM\ManyToOne(targetEntity="Application\Model\Questionnaire")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(onDelete="CASCADE", nullable=false)
+     * })
+     */
+    private $questionnaire;
+
+    /**
+     * @var Part
+     *
+     * @ORM\ManyToOne(targetEntity="Application\Model\Part")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(onDelete="CASCADE")
+     * })
+     */
+    private $part;
+
+    /**
+     * @var AbstractRule
+     *
+     * @ORM\ManyToOne(targetEntity="AbstractRule")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(onDelete="CASCADE", nullable=false)
+     * })
+     */
+    private $rule;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string")
+     */
+    private $justification;
+
+    /**
+     * Set questionnaire
+     *
+     * @param \Application\Model\Questionnaire $questionnaire
+     * @return QuestionnaireRule
+     */
+    public function setQuestionnaire(\Application\Model\Questionnaire $questionnaire)
+    {
+        $this->questionnaire = $questionnaire;
+
+        return $this;
+    }
+
+    /**
+     * Get questionnaire
+     *
+     * @return \Application\Model\Questionnaire
+     */
+    public function getQuestionnaire()
+    {
+        return $this->questionnaire;
+    }
+
+    /**
+     * Set part
+     *
+     * @param \Application\Model\Part $part
+     * @return QuestionnaireRule
+     */
+    public function setPart(\Application\Model\Part $part = null)
+    {
+        $this->part = $part;
+
+        return $this;
+    }
+
+    /**
+     * Get part
+     *
+     * @return \Application\Model\Part
+     */
+    public function getPart()
+    {
+        return $this->part;
+    }
+
+    /**
+     * Set rule
+     *
+     * @param AbstractRule $rule
+     * @return QuestionnaireRule
+     */
+    public function setRule(AbstractRule $rule)
+    {
+        $this->rule = $rule;
+
+        return $this;
+    }
+
+    /**
+     * Get rule
+     *
+     * @return AbstractRule
+     */
+    public function getRule()
+    {
+        return $this->rule;
+    }
+
+    /**
+     * Set justification
+     *
+     * @param string $justification
+     * @return QuestionnaireRule
+     */
+    public function setJustification($justification)
+    {
+        $this->justification = $justification;
+
+        return $this;
+    }
+
+    /**
+     * Get justification
+     *
+     * @return string
+     */
+    public function getJustification()
+    {
+        return $this->justification;
+    }
+
+}
