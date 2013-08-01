20d7e3752a8eb9a1f723429dc783c4ff8d2dcd07
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-08-01 17:45:31 +0900

    Add custom v() and w() debug method to dump variables

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,6 +5,8 @@ if (!ini_get('date.timezone')) {
     date_default_timezone_set('UTC');
 }
 
+require_once(__DIR__ . '/../module/debug.php');
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
