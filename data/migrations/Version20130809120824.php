1f763927b3bd9a630a88e2f3ce1c47596135feef
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-08-20 14:07:11 +0900

    Truncate table for easier migration
    This is bad practice, but data need to be re-imported anyway

--- data/migrations/Version20130809120824.php
+++ data/migrations/Version20130809120824.php
@@ -16,6 +16,9 @@ class Version20130809120824 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "postgresql", "Migration can only be executed safely on 'postgresql'.");
 
+        $this->addSql("TRUNCATE part CASCADE");
+        $this->addSql("TRUNCATE filter_rule CASCADE");
+        $this->addSql("TRUNCATE question CASCADE");
         $this->addSql("ALTER TABLE filter_rule ALTER part_id SET NOT NULL");
         $this->addSql("ALTER TABLE questionnaire_rule ALTER part_id SET NOT NULL");
         $this->addSql("ALTER TABLE part ADD is_total BOOLEAN NOT NULL");
