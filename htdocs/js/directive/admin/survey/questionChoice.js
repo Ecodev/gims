952aa7d94898a5f9b1137db0ca1f2d1cb632e796
Samuel Baptista <samuel.baptista@ecodev.ch>
2013-09-02 14:42:56 +0200

    Repair Contribute > Questionnaire > QuestionChoice display

--- htdocs/js/directive/admin/survey/questionChoice.js
+++ htdocs/js/directive/admin/survey/questionChoice.js
@@ -1,19 +1,20 @@
 angular.module('myApp.directives').directive('gimsChoiQuestion', function () {
     return {
         restrict: 'E',
-        template:   "<div class='row-fluid' ng-repeat='choice in question.choices'>"+
+        template:   "<div class='row-fluid'>"+
+                    "   <div class='span1 text-center' ng-repeat='part in question.parts'>"+
+                    "       {{part.name}}"+
+                    "   </div>"+
+                    "</div>"+
+                    "<div class='row-fluid' ng-repeat='choice in question.choices'>"+
                     "   <div class='span1 text-center' ng-repeat='part in question.parts'>"+
                     "       <input type='radio' name='{{part}}-{{question.id}}'/>"+
                     "   </div>"+
                     "   <span class='span9'>"+
                     "       {{choice.label}}"+
                     "   </span>"+
-                    "</div>"+
-                    "<div class='row-fluid'>"+
-                    "   <div class='span1 text-center' ng-repeat='part in question.parts'>"+
-                    "       {{part.name}}"+
-                    "   </div>"+
                     "</div>",
+
         scope:{
             question:'='
         },
