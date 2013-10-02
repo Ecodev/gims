0f9afecd1cf52e4071adb826f8515c63399d0871
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-10-02 22:51:57 +0900

    Log progress into file when importing all countries #2294

--- bin/import_all_country_data.sh
+++ bin/import_all_country_data.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-find data/cache/country_data -type f -name '*_12.xlsm' ! -name '~$*' -print -exec php htdocs/index.php import jmp {} \;
+echo 'Started. See import_all_country.log for progres...'
+time find data/cache/country_data -type f -name '*_12.xlsm' ! -name '~$*' -print -exec date --iso-8601=seconds \; -exec php htdocs/index.php import jmp {} \; &> import_all_country.log
