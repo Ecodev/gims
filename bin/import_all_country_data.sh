a6f11c530f375aeb6e7ed4cce0c0c5dd00755617
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-10-04 23:29:41 +0900

    Truncate before importing all countries

--- bin/import_all_country_data.sh
+++ bin/import_all_country_data.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo 'Started. See import_all_country.log for progres...'
+truncate -s 0 data/logs/all.log
 time find data/cache/country_data -type f -name '*_12.xlsm' ! -name '~$*' -print -exec date --iso-8601=seconds \; -exec php htdocs/index.php import jmp {} \; &> import_all_country.log
