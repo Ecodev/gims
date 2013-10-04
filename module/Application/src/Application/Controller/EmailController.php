1870fb274627d629db1eaf36a63ba8d9e8a8b9d7
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-10-04 19:30:54 +0900

    Render email content via View system #2295

--- module/Application/src/Application/Controller/EmailController.php
+++ module/Application/src/Application/Controller/EmailController.php
@@ -2,7 +2,6 @@
 
 namespace Application\Controller;
 
-
 use Zend\Mail;
 use Zend\Mail\Message;
 use Zend\Mail\Transport\Smtp as SmtpTransport;
@@ -18,7 +17,6 @@ class EmailController extends AbstractActionController
 
     use \Application\Traits\EntityManagerAware;
 
-
     /**
      *  Receive a questionnaire and a user role. Returne only users that have that role.
      */
@@ -27,15 +25,13 @@ class EmailController extends AbstractActionController
         $users = $this->getEntityManager()->getRepository('Application\Model\UserQuestionnaire')->getAllWithPermission('validator', 'questionnaire', $questionnaire);
 
         $selectedUsers = array();
-        foreach($users as $user){
+        foreach ($users as $user) {
             if ($user->getRole()->getName() == $wantedRole) {
                 array_push($selectedUsers, $user->getUser());
             }
         }
         return $selectedUsers;
     }
-
-
 
     /**
      * Notify questionnaire reporters if the questionnaire is again editable
@@ -46,10 +42,8 @@ class EmailController extends AbstractActionController
         $questionnaire = $this->getEntityManager()->getRepository('Application\Model\Questionnaire')->findOneById($questionnaireId);
 
         $users = $this->getUsersByRole($questionnaire, 'validator');
-        $this->sendMail($users, 'The questionnaire '.$questionnaire->getName().' has been re-opened.');
-
+        $this->sendMail($users, 'The questionnaire ' . $questionnaire->getName() . ' has been re-opened.');
     }
-
 
     /**
      * Notify questionnaire creator if (sent by validators)
@@ -62,11 +56,8 @@ class EmailController extends AbstractActionController
         $questionnaire = $this->getEntityManager()->getRepository('Application\Model\Questionnaire')->findOneById($questionnaireId);
 
         $creator = $questionnaire->getCreator();
-        $this->sendMail(array($creator), 'The questionnaire '.$questionnaire->getName().' has been validated.') ;
+        $this->sendMail(array($creator), 'The questionnaire ' . $questionnaire->getName() . ' has been validated.');
     }
-
-
-
 
     /**
      * Notify all questionnaire validators (dispatched by reporters)
@@ -78,52 +69,62 @@ class EmailController extends AbstractActionController
 
         $users = $this->getUsersByRole($questionnaire, 'validator');
 
-//        $renderer = new PhpRenderer();
-//        $resolver = new Resolver\AggregateResolver();
-//        $renderer->setResolver($resolver);
-//        $model = new ViewModel(array('test' => 'test ok'));
-//        $model->setTemplate('email/email');
-//        $content = $renderer->render($model);
-
-        $this->sendMail($users, 'The questionnaire '.$questionnaire->getName().' has been completed.');
-
+        $subject = 'GIMS - Questionnaire validated: ' . $questionnaire->getName();
+        $this->sendMail($users, $subject, new ViewModel(array('questionnaire' => $questionnaire)));
     }
 
-
-
-
-    public function sendMail($users, $subject, $content=null)
+    public function sendMail($users, $subject, ViewModel $model)
     {
+        static $emailCount = 0;
+
         if (count($users) > 0) {
 
             $config = $this->getServiceLocator()->get('Config');
 
-            // Setup SMTP transport using LOGIN authentication
-            $transport = new SmtpTransport();
-            $options   = new SmtpOptions( $config['smtp']);
-            $transport->setOptions($options);
+            foreach ($users as $user) {
 
-            $mail = new Mail\Message();
-            $mail->setSubject($subject);
-            $mail->setBody($subject);
-            $mail->setFrom('webmaster@gimsinitiative.org', 'Gims project');
+                $renderer = $this->getServiceLocator()->get('ViewRenderer');
 
-            if (isset($config['emailOverride']) && !empty($config['emailOverride'])) {
-                $mail->addTo($config['emailOverride']);
-            } else {
-                foreach ($users as $user) {
-                    $mail->addTo($user->getEmail(), $user->getDisplayName());
+                // First render the view
+                $template = 'application/email/' . $this->getRequest()->getParam('action');
+                $model->setTemplate($template);
+                $model->setVariable('user', $user);
+                $partialContent = $renderer->render($model);
+
+                // Then inject it into layout
+                $modelLayout = new ViewModel(array($model->captureTo() => $partialContent));
+                $modelLayout->setTemplate('application/email/email');
+                $modelLayout->setVariable('user', $user);
+                $content = $renderer->render($modelLayout);
+
+                // Setup SMTP transport using LOGIN authentication
+                $transport = new SmtpTransport();
+                $options = new SmtpOptions($config['smtp']);
+                $transport->setOptions($options);
+
+                $mail = new Mail\Message();
+                $mail->setSubject($subject);
+                $mail->setBody($content);
+                $mail->setFrom('webmaster@gimsinitiative.org', 'Gims project');
+
+                $email = $user->getEmail();
+                if (isset($config['emailOverride'])) {
+                    $email = $config['emailOverride'];
                 }
+
+                if ($email) {
+                    $mail->addTo($email, $user->getDisplayName());
+                    $transport->send($mail);
+                }
+
+                $dateNow = new \DateTime();
+                $dateNow = $dateNow->format('c');
+
+                $log = 'data/logs/emails/' . $dateNow . '_' . str_pad($emailCount++, 3, '0', STR_PAD_LEFT) . '_' . $email . '.html';
+                file_put_contents($log, $mail->getBody());
+
+                echo 'email sent to: ' . $user->getName() . "\t" . $email . "\t" . $subject . "\n";
             }
-
-            $dateNow = new \DateTime();
-            $dateNow = $dateNow->format('Y-m-d_H:i:s,u');
-
-            $log = 'data/logs/emails/' . $dateNow .'.html';
-            file_put_contents($log, $mail->toString());
-
-            $transport->send($mail);
-            return $mail->toString()."\n";
         }
     }
 
