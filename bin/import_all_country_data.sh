17b2723d589572b9717577a2306a629f750f357b
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-10-18 11:16:36 +0900

    Save log in appropriate folder

--- bin/import_all_country_data.sh
+++ bin/import_all_country_data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo 'Started. See import_all_country.log for progres...'
+echo 'Started. See data/logs/import_all_country.log for progress...'
 truncate -s 0 data/logs/all.log
-time find data/cache/country_data -type f -name '*_12.xlsm' ! -name '~$*' -print -exec date --iso-8601=seconds \; -exec php htdocs/index.php import jmp {} \; &> import_all_country.log
+time find data/cache/country_data -type f -name '*_12.xlsm' ! -name '~$*' -print -exec date --iso-8601=seconds \; -exec php htdocs/index.php import jmp {} \; &> data/logs/import_all_country.log
