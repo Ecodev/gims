bcad0ae8f9d5f72b223507ceba834fe874e99504
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-11-15 15:32:19 +0900

    Use proper control-label class to have colored labels in case of errors #2278

--- htdocs/js/service/userModal.js
+++ htdocs/js/service/userModal.js
@@ -19,46 +19,47 @@ angular.module('myApp.services')
                             "<div class='modal-body'>" +
                             '<form name="myForm">' +
                             '   <div class="form-group" ng-class="{\'has-error\': myForm.name.$invalid}">' +
-                            '       <label for="localUser.name">Name (first name and last name)</label>' +
+                            '       <label class="control-label" for="localUser.name">Name (first name and last name)</label>' +
                             '       <input id="localUser.name" type="text" name="name" ng-model="localUser.name" required ng-minlength="3" ng-disabled="localUser.lastLogin" />' +
                             '       <span ng-show="myForm.name.$error.required" class="help-block">Required</span>' +
                             '       <span ng-show="myForm.name.$error.minlength" class="help-block">It must be at least 3 characters long</span>' +
                             '   </div>' +
                             '   <div class="form-group" ng-class="{\'has-error\': myForm.email.$invalid}">' +
-                            '       <label for="localUser.email">Email</label>' +
+                            '       <label class="control-label" for="localUser.email">Email</label>' +
                             '       <input id="localUser.email" type="email" name="email" ng-model="localUser.email" required ng-disabled="localUser.lastLogin" />' +
                             '       <span ng-show="myForm.email.$error.required" class="help-block">Required</span>' +
+                            '       <span ng-show="myForm.email.$error.email" class="help-block">Enter a valid email address</span>' +
                             '   </div>' +
                             '   <div class="form-group" ng-class="{\'has-error\': myForm.phone.$invalid}">' +
-                            '       <label for="localUser.phone">Phone</label>' +
+                            '       <label class="control-label" for="localUser.phone">Phone</label>' +
                             '       <input id="localUser.phone" type="text" name="phone" ng-model="localUser.phone" ng-disabled="localUser.lastLogin" />' +
                             '   </div>' +
                             '   <div class="form-group" ng-class="{\'has-error\': myForm.skype.$invalid}">' +
-                            '       <label for="localUser.skype">Skype</label>' +
+                            '       <label class="control-label" for="localUser.skype">Skype</label>' +
                             '       <input id="localUser.skype" type="text" name="skype" ng-model="localUser.skype" ng-disabled="localUser.lastLogin" />' +
                             '   </div>' +
                             '   <div class="form-group" ng-class="{\'has-error\': myForm.job.$invalid}">' +
-                            '       <label for="localUser.job">Job title</label>' +
+                            '       <label class="control-label" for="localUser.job">Job title</label>' +
                             '       <input id="localUser.job" type="text" name="job" ng-model="localUser.job" ng-disabled="localUser.lastLogin" />' +
                             '   </div>' +
                             '   <div class="form-group" ng-class="{\'has-error\': myForm.ministry.$invalid}">' +
-                            '       <label for="localUser.ministry">Ministry / Departement</label>' +
+                            '       <label class="control-label" for="localUser.ministry">Ministry / Departement</label>' +
                             '       <input id="localUser.ministry" type="text" name="ministry" ng-model="localUser.ministry" ng-disabled="localUser.lastLogin" />' +
                             '   </div>' +
                             '   <div class="form-group" ng-class="{\'has-error\': myForm.address.$invalid}">' +
-                            '       <label for="localUser.address">Address</label>' +
+                            '       <label class="control-label" for="localUser.address">Address</label>' +
                             '       <textarea id="localUser.address" type="text" name="address" ng-model="localUser.address" ng-disabled="localUser.lastLogin" ></textarea>' +
                             '   </div>' +
                             '   <div class="form-group" ng-class="{\'has-error\': myForm.zip.$invalid}">' +
-                            '       <label for="localUser.zip">ZIP</label>' +
+                            '       <label class="control-label" for="localUser.zip">ZIP</label>' +
                             '       <input id="localUser.zip" type="text" name="zip" ng-model="localUser.zip" ng-disabled="localUser.lastLogin" />' +
                             '   </div>' +
                             '   <div class="form-group" ng-class="{\'has-error\': myForm.city.$invalid}">' +
-                            '       <label for="localUser.city">City</label>' +
+                            '       <label class="control-label" for="localUser.city">City</label>' +
                             '       <input id="localUser.city" type="text" name="city" ng-model="localUser.city" ng-disabled="localUser.lastLogin" />' +
                             '   </div>' +
                             '   <div class="form-group" ng-class="{\'has-error\': myForm.country.$invalid}">' +
-                            '       <label for="country">Country</label>' +
+                            '       <label class="control-label" for="country">Country</label>' +
                             '       <gims-select id="country" api="country" name="country" model="localUser.country" style="width:100%" disabled="localUser.lastLogin" ></gims-select>' +
                             '   </div>' +
                             '</form>' +
