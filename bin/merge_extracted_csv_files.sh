76fc18b6c93a6f83ad6221b942f76ddb517757fa
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-11-19 18:45:10 +0900

    Reorder extracted columns according to GIMS format #2359

--- bin/merge_extracted_csv_files.sh
+++ bin/merge_extracted_csv_files.sh
@@ -2,3 +2,7 @@
 
 { tail -q -n +1  *Water.csv ; } > _ALL_WATER.csv
 { tail -q -n +1  *Sanitation.csv ; } > _ALL_SANITATION.csv
+
+# Reorder columns to match format of GIMS output
+awk -F "," '{print $1 "," $2 "," $3 "," $4 "," $7 "," $5 "," $9 ",\"\",\"\"," $13 "," $11 "," $15 ",\"\",\"\"," $19 "," $17 "," $21 ",\"\",\"\"" }' _ALL_WATER.csv > _ALL_WATER_REORDERED.csv
+awk -F "," '{print $1 "," $2 "," $3 "," $4 "," $7 "," $5 ",\"\"," $9 ",\"\"," $11 "," $15 "," $13 ",\"\"," $17 ",\"\"," $19 "," $23 "," $21 ",\"\"," $25 ",\"\"," $27 }' _ALL_SANITATION.csv > _ALL_SANITATION_REORDERED.csv
