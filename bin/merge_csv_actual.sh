3ac7ed7237d72a4950b9cb87b4419d2a0a6d20da
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2013-12-17 16:48:42 +0900

    Simplify merging scripts

--- bin/merge_csv_actual.sh
+++ bin/merge_csv_actual.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-{ head -q -n 1 *Uganda\ -\ Water.csv && tail -q -n +2  *Water.csv ; } > _ALL_WATER.csv
-{ head -q -n 1 *Uganda\ -\ Sanitation.csv && tail -q -n +2  *Sanitation.csv ; } > _ALL_SANITATION.csv
+{ tail -q -n +2  *Water.csv ; } > _WATER_ACTUAL.csv
+{ tail -q -n +2  *Sanitation.csv ; } > _SANITATION_ACTUAL.csv
