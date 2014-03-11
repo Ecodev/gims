809faf46bff7f15a27e8cc3016fcbfe115993eb3
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-03-11 11:56:19 +0100

    Reset sequences dynamically instead of hardcoding tables #2560

--- bin/import_test_data.sh
+++ bin/import_test_data.sh
@@ -15,26 +15,15 @@ SQL_PATH="`pwd`/tests/data/sql"
 ./vendor/bin/doctrine-module dbal:run-sql "COPY choice FROM '$SQL_PATH/choice.sql';"
 
 # Reset sequences values
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('answer_id_seq', MAX(id) ) FROM answer;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('choice_id_seq', MAX(id) ) FROM choice;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('country_id_seq', MAX(id) ) FROM country;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('filter_id_seq', MAX(id) ) FROM filter;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('filter_questionnaire_usage_id_seq', MAX(id) ) FROM filter_questionnaire_usage;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('filter_set_id_seq', MAX(id) ) FROM filter_set;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('filter_geoname_usage_id_seq', MAX(id) ) FROM filter_geoname_usage;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('geoname_id_seq', MAX(id) ) FROM geoname;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('part_id_seq', MAX(id) ) FROM part;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('permission_id_seq', MAX(id) ) FROM permission;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('population_id_seq', MAX(id) ) FROM population;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('question_id_seq', MAX(id) ) FROM question;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('questionnaire_id_seq', MAX(id) ) FROM questionnaire;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('questionnaire_usage_id_seq', MAX(id) ) FROM questionnaire_usage;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('role_id_seq', MAX(id) ) FROM role;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('rule_id_seq', MAX(id) ) FROM rule;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('survey_id_seq', MAX(id) ) FROM survey;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('user_id_seq', MAX(id) ) FROM \"user\";"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('user_questionnaire_id_seq', MAX(id) ) FROM user_questionnaire;"
-./vendor/bin/doctrine-module dbal:run-sql "SELECT SETVAL('user_survey_id_seq', MAX(id) ) FROM user_survey;"
+./vendor/bin/doctrine-module dbal:run-sql --ansi "SELECT  'SELECT SETVAL(' ||quote_literal(quote_ident(PGT.schemaname)|| '.'||quote_ident(S.relname))|| ', MAX(' ||quote_ident(C.attname)|| ') ) FROM ' ||quote_ident(PGT.schemaname)|| '.'||quote_ident(T.relname)|| ';'
+FROM pg_class AS S, pg_depend AS D, pg_class AS T, pg_attribute AS C, pg_tables AS PGT
+WHERE S.relkind = 'S'
+    AND S.oid = D.objid
+    AND D.refobjid = T.oid
+    AND D.refobjid = C.attrelid
+    AND D.refobjsubid = C.attnum
+    AND T.relname = PGT.tablename
+ORDER BY S.relname;" | grep -o "'SELECT.*'" | sed "s/'/\"/g" | sed "s/&#39;/'/g" | xargs -L 1 ./vendor/bin/doctrine-module dbal:run-sql
 
 # Import additional countries
 time php htdocs/index.php import jmp data/cache/country_data/Afghanistan_13.xlsm # for importing NULL instead of ignoring non-existing formulas
