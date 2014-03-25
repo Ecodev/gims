9b271e186ae695f099fc3f329c048a2d32338950
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-03-25 11:55:54 +0100

    debug travis

--- bin/build.sh
+++ bin/build.sh
@@ -18,40 +18,8 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 cd $DIR/..
 
-echo "Installing git hooks..."
-ln -fs ../../bin/pre-commit.sh .git/hooks/pre-commit
-
 echo "Updating git submodules..."
 git submodule update --init --recursive --force
 
-echo "Updating Bower packages..."
-bower install
-
 echo "Updating all PHP dependencies via composer..."
 ./composer.phar install --dev --optimize-autoloader
-
-echo "Updating database..."
-./vendor/bin/doctrine-module migrations:migrate --no-interaction
-./vendor/bin/doctrine-module orm:generate-proxies
-
-echo "Clean Zend cache..."
-rm -vf data/cache/*cache*.php
-
-echo "Compiling CSS..."
-compass compile -s compressed --force
-
-echo "Compiling JavaScript..."
-TARGET="tmp/application.js"
-cd htdocs
-rm -f $TARGET
-
-# First do lib, then our own code
-for jsDir in lib/autoload/ js/ ; do
-    for file in `find -L $jsDir -type f -name '*.js' | sort` ; do
-        echo "$file"
-        D=`dirname $file`
-        mkdir -p "tmp/$D"
-        more "$file" | ngmin | uglifyjs - -o "tmp/$file" # uglify the code
-        cat "tmp/$file" >> $TARGET # concatenate in a single file
-    done
-done
--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Installing Compass..."
-GEMS="gem install --quiet --no-rdoc --no-ri sass compass oily_png bootstrap-sass"
-if [[ -z $TRAVIS ]]; then # If not on Travis, need to use sudo
-   GEMS="sudo $GEMS"
-fi
-`$GEMS`
-
 # Exit script on any error
 set -e
 
@@ -16,56 +9,11 @@ sudo apt-get install -qq software-properties-common # to get next command: add-a
 sudo add-apt-repository --yes ppa:ubuntugis/ubuntugis-unstable
 sudo add-apt-repository --yes ppa:chris-lea/node.js
 sudo apt-get -qq update
-sudo apt-get -qq install postgresql-9.1-postgis-2.0 rubygems nodejs apache2 php5-pgsql php5-cli php5-gd php5-mcrypt php5-intl php5-json
+sudo apt-get -qq install postgresql-9.1-postgis-2.0 php5-pgsql php5-cli php5-gd php5-mcrypt php5-intl php5-json
 
-echo "Installing JS testing tools..."
-sudo npm --global --quiet install karma karma-ng-scenario phantomjs uglify-js ngmin bower jshint
-
-echo "Installing php-cs-fixer..."
-sudo wget http://cs.sensiolabs.org/get/php-cs-fixer.phar -O /usr/local/bin/php-cs-fixer
-sudo chmod a+x /usr/local/bin/php-cs-fixer
-
-# For Travis only
-if [[ ! -z $TRAVIS ]]; then
-    # we replace pre-installed PhantomJS with npm version (more recent)
-    sudo ln -sf "`sudo npm bin -g`/phantomjs" `which phantomjs`
-
-    # Configure xdebug with a higher max_nesting_level
-    for file in `find /home/vagrant/.phpenv -name 'xdebug.ini'`; do echo "xdebug.max_nesting_level=5000" >> $file; done;
-fi
 
 # For Travis CI, or full local install, we need more configuration (Apache and database)
 if [[ "$1" = "configure" ]]; then
-    echo "Configuring Apache..."
-    WEBROOT="$(pwd)/htdocs"
-    CGIROOT=`dirname "$(which php-cgi)"`
-    sudo echo "<VirtualHost *:80>
-            DocumentRoot $WEBROOT
-            <Directory />
-                    Options FollowSymLinks
-                    AllowOverride All
-            </Directory>
-            <Directory $WEBROOT >
-                    Options Indexes FollowSymLinks MultiViews
-                    AllowOverride All
-                    Order allow,deny
-                    allow from all
-            </Directory>
-
-            # Configure PHP as CGI
-            ScriptAlias /local-bin $CGIROOT
-            DirectoryIndex index.php index.html
-            AddType application/x-httpd-php5 .php
-            Action application/x-httpd-php5 '/local-bin/php-cgi'
-    </VirtualHost>" | sudo tee /etc/apache2/sites-available/default > /dev/null
-
-    sudo a2enmod actions
-    sudo a2enmod rewrite
-    sudo a2enmod headers
-    sudo service apache2 restart
-
-    echo "Configuring custom domain..."
-    echo "127.0.0.1 gims.lan" | sudo tee --append /etc/hosts
 
     echo "Init database..."
     cp config/autoload/local.php.dist config/autoload/local.php
