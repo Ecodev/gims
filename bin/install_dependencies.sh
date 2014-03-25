a841650b0218ac0c6d4c012b15757a098e1e6742
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-03-25 17:40:39 +0100

    Use specific version of Karma

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -19,7 +19,7 @@ sudo apt-get -qq update
 sudo apt-get -qq install postgresql-9.1-postgis-2.0 rubygems nodejs apache2 php5-pgsql php5-cli php5-gd php5-mcrypt php5-intl php5-json
 
 echo "Installing JS testing tools..."
-sudo npm --global --quiet install karma karma-ng-scenario phantomjs uglify-js ngmin bower jshint
+sudo npm --global --quiet install karma@0.10.9 karma-ng-scenario@0.1.0 phantomjs uglify-js ngmin bower jshint
 
 echo "Installing php-cs-fixer..."
 sudo wget http://cs.sensiolabs.org/get/php-cs-fixer.phar -O /usr/local/bin/php-cs-fixer
