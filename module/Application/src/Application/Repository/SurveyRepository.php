0bf8a7c9e0025c4faa615bfb8b62d3c4c73784f3
Sam Baptista <samuel.baptista@ecodev.ch>
2014-04-15 11:11:32 +0200

    Implements the ability to add questoinnaires in new GUI filter/questionnaires #2641

--- module/Application/src/Application/Repository/SurveyRepository.php
+++ module/Application/src/Application/Repository/SurveyRepository.php
@@ -15,27 +15,11 @@ class SurveyRepository extends AbstractRepository
                 ->addOrderBy('survey.year', 'DESC')
                 ->addOrderBy('survey.name', 'ASC')
         ;
-        
+
         $this->addSearch($qb, $search);
         $this->addPermission($qb, 'survey', \Application\Model\Permission::getPermissionName($this, $action));
 
         return $qb->getQuery()->getResult();
-    }
-
-    /**
-     * Returns all items with read access
-     * @return array
-     */
-    public function agetAllWithPermission($action = 'read')
-    {
-        $permissionDql = $this->addPermission('survey', 'Survey-read');
-        $query = $this->getEntityManager()->createQuery("SELECT survey
-            FROM Application\Model\Survey survey
-            $permissionDql
-            ORDER BY survey.year DESC, survey.name ASC"
-        );
-
-        return $query->getResult();
     }
 
 }
