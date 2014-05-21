0b5d8ead3f92c2c868d4e73eda6d536efd14e675
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-05-21 15:30:51 +0900

    Avoid loading status flickering when chaining requests #2581

--- htdocs/js/service/requestNotification.js
+++ htdocs/js/service/requestNotification.js
@@ -3,68 +3,69 @@
  */
 angular.module('myApp.services').provider('requestNotification', function() {
 
-    // This is where we keep subscribed listeners
-    var onRequestStartedListeners = [];
-    var onRequestEndedListeners = [];
-    var onResponseErrorListeners = [];
-
-    // This is a utility to easily increment the request count
-    var count = 0;
-    var requestCounter = {
-        increment: function() {
-            count++;
-        },
-        decrement: function() {
-            if (count > 0) {
-                count--;
-            }
-        },
-        getCount: function() {
-            return count;
-        }
-    };
-
-    // Subscribe to be notified when request starts and ends
-    this.subscribeOnRequest = function(listenerStarted, listenerEnded) {
-        onRequestStartedListeners.push(listenerStarted);
-        onRequestEndedListeners.push(listenerEnded);
-    };
-
-    // Tell the provider, that the request has started.
-    this.fireRequestStarted = function() {
-        requestCounter.increment();
-        angular.forEach(onRequestStartedListeners, function(listener) {
-            listener();
-        });
-    };
-
-    this.fireRequestEnded = function() {
-        requestCounter.decrement();
-        angular.forEach(onRequestEndedListeners, function(listener) {
-            listener();
-        });
-    };
-
-    // this is a complete analogy to the Request START
-    this.subscribeOnResponseError = function(listener) {
-        onResponseErrorListeners.push(listener);
-    };
-    this.fireResponseError = function(response) {
-        angular.forEach(onResponseErrorListeners, function(listener) {
-            listener(response);
-        });
-    };
-
     //This will be returned as a service
-    this.$get = function() {
-        var that = this;
-        // just pass all the
+    this.$get = function($timeout) {
+
+        // This is where we keep subscribed listeners
+        var onRequestStartedListeners = [];
+        var onRequestEndedListeners = [];
+        var onResponseErrorListeners = [];
+
+        // This is a utility to easily increment the request count
+        var count = 0;
+        var requestCounter = {
+            increment: function() {
+                count++;
+            },
+            decrement: function() {
+                if (count > 0) {
+                    count--;
+                }
+            },
+            getCount: function() {
+                return count;
+            }
+        };
+
+        // Tell the provider, that the request has started.
+        this.fireRequestStarted = function() {
+
+            requestCounter.increment();
+            angular.forEach(onRequestStartedListeners, function(listener) {
+                listener();
+            });
+        };
+
+        this.fireRequestEnded = function() {
+            $timeout(function() {
+                requestCounter.decrement();
+                angular.forEach(onRequestEndedListeners, function(listener) {
+                    listener();
+                });
+            }, 50); // Wait a short time to give a chance to code to start another request without flickering loading state
+        };
+
+        this.fireResponseError = function(response) {
+            angular.forEach(onResponseErrorListeners, function(listener) {
+                listener(response);
+            });
+        };
+
+        // Subscribe to be notified when request starts and ends
+        function subscribeOnRequest(listenerStarted, listenerEnded) {
+            onRequestStartedListeners.push(listenerStarted);
+            onRequestEndedListeners.push(listenerEnded);
+        }
+
+        // Subscribe to be notified when request errors
+        function subscribeOnResponseError(listener) {
+            onResponseErrorListeners.push(listener);
+        }
+
+        // Expose service's public function
         return {
-            subscribeOnRequest: that.subscribeOnRequest,
-            subscribeOnResponseError: that.subscribeOnResponseError,
-            fireRequestEnded: that.fireRequestEnded,
-            fireRequestStarted: that.fireRequestStarted,
-            fireResponseError: that.fireResponseError,
+            subscribeOnRequest: subscribeOnRequest,
+            subscribeOnResponseError: subscribeOnResponseError,
             getRequestCount: requestCounter.getCount
         };
     };
