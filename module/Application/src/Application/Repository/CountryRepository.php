4e35ae9983032b7d945a9d10f2ff42d3c7a59b2b
Sam Baptista <samuel.baptista@ecodev.ch>
2014-06-09 14:32:38 +0200

    Order countries alphabetically #2732

--- module/Application/src/Application/Repository/CountryRepository.php
+++ module/Application/src/Application/Repository/CountryRepository.php
@@ -6,4 +6,8 @@ class CountryRepository extends AbstractRepository
 {
     use Traits\OrderedByName;
 
+    public function getAllWithPermission($action = 'read', $search = null)
+    {
+        return $this->findAll();
+    }
 }
