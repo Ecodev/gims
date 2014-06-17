e99d55bc0dfeb56f4987e052e503fb481c93c5a3
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-06-17 21:43:23 +0900

    Also import South Sudan #2698

--- bin/import_all_country_data.sh
+++ bin/import_all_country_data.sh
@@ -2,4 +2,4 @@
 
 echo 'Started. See data/logs/import_all_country.log for progress...'
 > data/logs/all.log
-time find -L data/cache/country_data -type f -name '*_13.xlsm' ! -name '~$*' -print -exec date \; -exec php htdocs/index.php import jmp {} \; &> data/logs/import_all_country.log
+time find -L data/cache/country_data -type f  \( -name '*_13.xlsm' -o -name '*_13_All_Pop.xlsm' \) ! -name '~$*' -print -exec date \; -exec php htdocs/index.php import jmp {} \; &> data/logs/import_all_country.log
