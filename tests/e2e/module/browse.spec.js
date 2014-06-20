e38b739a942a4f107a0c92e32ccf0c2cf0612021
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-06-20 15:39:23 +0900

    Invert order of /browse icons to better represent data workflow

--- tests/e2e/module/browse.spec.js
+++ tests/e2e/module/browse.spec.js
@@ -8,13 +8,14 @@ describe('browse', function() {
     });
 
     it('should render about when user navigates to /browse', function() {
-        expect(element(by.css('[ng-view] .browse .col-md-4:nth-child(1)')).getText()).toContain("Maps");
-        expect(element(by.css('[ng-view] .browse .col-md-4:nth-child(2)')).getText()).toContain("Charts");
 
-        var text = element(by.css('[ng-view] .browse .col-md-4:nth-child(3)')).getText();
+        var text = element(by.css('[ng-view] .browse .col-md-4:nth-child(1)')).getText();
         expect(text).toContain("Filters");
         expect(text).toContain("Questionnaires");
         expect(text).toContain("Countries");
+
+        expect(element(by.css('[ng-view] .browse .col-md-4:nth-child(2)')).getText()).toContain("Charts");
+        expect(element(by.css('[ng-view] .browse .col-md-4:nth-child(3)')).getText()).toContain("Maps");
     });
 
 });
