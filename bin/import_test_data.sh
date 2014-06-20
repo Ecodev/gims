74296b4320c4a0c498ce7cd5491ec726bcb7f77e
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-06-20 10:28:29 +0900

    Support empty table when reseting sequences #2698

--- bin/import_test_data.sh
+++ bin/import_test_data.sh
@@ -15,7 +15,7 @@ SQL_PATH="`pwd`/tests/data/sql"
 ./vendor/bin/doctrine-module dbal:run-sql "COPY choice FROM '$SQL_PATH/choice.sql';"
 
 # Reset sequences values
-./vendor/bin/doctrine-module dbal:run-sql --ansi "SELECT  'SELECT SETVAL(' ||quote_literal(quote_ident(PGT.schemaname)|| '.'||quote_ident(S.relname))|| ', MAX(' ||quote_ident(C.attname)|| ') ) FROM ' ||quote_ident(PGT.schemaname)|| '.'||quote_ident(T.relname)|| ';'
+./vendor/bin/doctrine-module dbal:run-sql --ansi "SELECT  'SELECT SETVAL(' ||quote_literal(quote_ident(PGT.schemaname)|| '.'||quote_ident(S.relname))|| ', COALESCE(MAX(' ||quote_ident(C.attname)|| '), 1) ) FROM ' ||quote_ident(PGT.schemaname)|| '.'||quote_ident(T.relname)|| ';'
 FROM pg_class AS S, pg_depend AS D, pg_class AS T, pg_attribute AS C, pg_tables AS PGT
 WHERE S.relkind = 'S'
     AND S.oid = D.objid
