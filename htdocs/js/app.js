f55427a96d4464a2a9d1f52d7cbcfdce9a2c1aee
Sam Baptista <samuel.baptista@ecodev.ch>
2014-06-27 13:13:14 +0200

    Update questionnaires client urls routes : /contribute/[glass/jmp/nsa] #2731

--- htdocs/js/app.js
+++ htdocs/js/app.js
@@ -48,8 +48,10 @@ angular.module('myApp', [
             $routeProvider.when('/admin/rule/edit/:id', {templateUrl: '/template/admin/rule/crud', controller: 'Admin/Rule/CrudCtrl'});
             $routeProvider.when('/admin/rule/new', {templateUrl: '/template/admin/rule/crud', controller: 'Admin/Rule/CrudCtrl'});
             $routeProvider.when('/browse/table/filter', {templateUrl: '/template/browse/table/filter', controller: 'Browse/FilterCtrl', reloadOnSearch: false});
-            $routeProvider.when('/contribute/questionnaire', {templateUrl: '/template/browse/table/filter', controller: 'Browse/FilterCtrl', reloadOnSearch: false});
-            $routeProvider.when('/contribute/questionnaire/glass/:id', {templateUrl: '/template/contribute/glass', controller: 'Contribute/GlassCtrl'});
+            $routeProvider.when('/contribute/jmp', {templateUrl: '/template/browse/table/filter', controller: 'Browse/FilterCtrl', reloadOnSearch: false});
+            $routeProvider.when('/contribute/nsa', {templateUrl: '/template/browse/table/filter', controller: 'Browse/FilterCtrl', reloadOnSearch: false});
+            $routeProvider.when('/contribute/glass/', {templateUrl: '/template/contribute/glass', controller: 'Contribute/GlassCtrl'});
+            $routeProvider.when('/contribute/glass/:id', {templateUrl: '/template/contribute/glass', controller: 'Contribute/GlassCtrl'});
 
             $routeProvider.otherwise({redirectTo: '/home'});
 
