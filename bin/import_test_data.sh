18ee25a22e802f6ce43f051a1dbeabfc37237882
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-07-02 15:43:27 +0900

    Group tasks together when importing test data to fail earlier #2769

--- bin/import_test_data.sh
+++ bin/import_test_data.sh
@@ -4,8 +4,6 @@ set -e
 > data/logs/all.log
 ./vendor/bin/phing load-data -DdumpFile=population.backup.gz
 
-time php htdocs/index.php import jmp data/cache/country_data/Bangladesh_13.xlsm
-
 # Reload partial dumps
 SQL_PATH="`pwd`/tests/data/sql"
 ./vendor/bin/doctrine-module dbal:run-sql "COPY \"user\" FROM '$SQL_PATH/user.sql';"
@@ -17,7 +15,8 @@ SQL_PATH="`pwd`/tests/data/sql"
 # Reset sequences values
 ./vendor/bin/phing reset-sequences
 
-# Import additional countries
+# Import countries
+time php htdocs/index.php import jmp data/cache/country_data/Bangladesh_13.xlsm
 time php htdocs/index.php import jmp data/cache/country_data/Afghanistan_13.xlsm # for importing NULL instead of ignoring non-existing formulas
 time php htdocs/index.php import jmp data/cache/country_data/Azerbaijan_13.xlsm   # for recursive formulas and interdependence during import
 time php htdocs/index.php import jmp data/cache/country_data/Germany_13.xlsm  # for developed countries rules
@@ -28,7 +27,7 @@ time php htdocs/index.php import jmp data/cache/country_data/Saudi_arabia_13.xls
 ./vendor/bin/doctrine-module dbal:run-sql "INSERT INTO user_survey (user_id, role_id, survey_id) SELECT 1, 7, survey.id FROM SURVEY;"
 ./vendor/bin/doctrine-module dbal:run-sql "INSERT INTO user_questionnaire (user_id, role_id, questionnaire_id) SELECT 1, 3, questionnaire.id FROM questionnaire;"
 ./vendor/bin/doctrine-module dbal:run-sql "INSERT INTO user_filter_set (role_id, user_id, filter_set_id) SELECT 6, 1, filter_set.id from filter_set;"
-./vendor/bin/doctrine-module dbal:run-sql "UPDATE filter SET creator_id=1 WHERE creator_id IS NULL;"
+./vendor/bin/doctrine-module dbal:run-sql "UPDATE filter SET creator_id = 1 WHERE creator_id IS NULL;"
 
 # Dump new database content
 ./vendor/bin/phing dump-data -DdumpFile=tests/data/db.backup.gz
