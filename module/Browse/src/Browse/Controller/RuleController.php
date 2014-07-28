5b0aa9f6105f5ddeae12f842724144da02ce769c
Sam Baptista <samuel.baptista@ecodev.ch>
2014-07-28 11:55:22 +0200

    Remove useless template #2829

--- module/Browse/src/Browse/Controller/RuleController.php
+++ module/Browse/src/Browse/Controller/RuleController.php
@@ -7,16 +7,6 @@ use Zend\View\Model\ViewModel;
 class RuleController extends \Application\Controller\AbstractAngularActionController
 {
 
-    public function indexAction()
-    {
-        return new ViewModel();
-    }
-
-    public function crudAction()
-    {
-        return new ViewModel();
-    }
-
     public function wizardAction()
     {
         return new ViewModel();
