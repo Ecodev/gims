4800ebac7443e1f882b9ae88c84ab06bd6188d44
Sam Baptista <samuel.baptista@ecodev.ch>
2014-09-05 16:41:45 +0200

    Move export button to top tool bar and method position #2792

--- htdocs/js/controller/browse/table/questionnaire.js
+++ htdocs/js/controller/browse/table/questionnaire.js
@@ -32,34 +32,6 @@ angular.module('myApp').controller('Browse/Table/QuestionnaireCtrl', function($s
         }
     });
 
-    /**
-     * Return a parameter in a list of objects (or single object)
-     * Almost could use _.pluck but the loop in pluck dont use angular.foreach and uses some angular function as object. pluck can't neither differentiate if server returns list or single object
-     * @param data
-     * @param attribute
-     * @param permission
-     * @returns {Array}
-     */
-    var getAttribute = function(data, attribute, permission) {
-        var list = [];
-        if (_.isArray(data)) {
-            angular.forEach(data, function(obj) {
-                list = list.concat(obj[attribute]);
-            });
-        } else if (data) {
-            list = data[attribute];
-        }
-
-        list = _.filter(list, function(l) {
-            if (!permission || permission && l && (!l.permissions || l.permissions && l.permissions[permission])) {
-                return true;
-            }
-            return false;
-        });
-
-        return list;
-    };
-
     $scope.$watch('tabs.geonames', function() {
         if ($scope.tabs.geonames && $scope.tabs.geonames.length) {
             $scope.tabs.questionnaires = [];
@@ -120,5 +92,33 @@ angular.module('myApp').controller('Browse/Table/QuestionnaireCtrl', function($s
             });
         }
     }, 300);
+
+    /**
+     * Return a parameter in a list of objects (or single object)
+     * Almost could use _.pluck but the loop in pluck dont use angular.foreach and uses some angular function as object. pluck can't neither differentiate if server returns list or single object
+     * @param data
+     * @param attribute
+     * @param permission
+     * @returns {Array}
+     */
+    var getAttribute = function(data, attribute, permission) {
+        var list = [];
+        if (_.isArray(data)) {
+            angular.forEach(data, function(obj) {
+                list = list.concat(obj[attribute]);
+            });
+        } else if (data) {
+            list = data[attribute];
+        }
+
+        list = _.filter(list, function(l) {
+            if (!permission || permission && l && (!l.permissions || l.permissions && l.permissions[permission])) {
+                return true;
+            }
+            return false;
+        });
+
+        return list;
+    };
 
 });
