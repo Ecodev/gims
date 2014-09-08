61cae4ee207c86261c4fa1e15aa7d01be6e09147
Sam Baptista <samuel.baptista@ecodev.ch>
2014-09-08 12:38:30 +0200

    Add tests to cover /api/table/country #2792

--- tests/ApiTest/Controller/TableControllerTest.php
+++ tests/ApiTest/Controller/TableControllerTest.php
@@ -52,4 +52,21 @@ class TableControllerTest extends \ApplicationTest\Controller\AbstractController
         $this->assertNumericJson($expectedJson, $this->getResponse()->getContent(), $message, $logFile);
     }
 
+    public function getValidDataCountryProvider()
+    {
+        return new \ApiTest\JsonFileIterator('data/api/table/country');
+    }
+
+    /**
+     * @dataProvider getValidDataCountryProvider
+     * @group LongTest
+     */
+    public function testGetValidDataCountry($params, $expectedJson, $message, $logFile)
+    {
+        $this->dispatch('/api/table/country?' . $params, Request::METHOD_GET);
+
+        $this->assertResponseStatusCode(200);
+        $this->assertNumericJson($expectedJson, $this->getResponse()->getContent(), $message, $logFile);
+    }
+
 }
