5790317d52908f94a0fc2b4ec53a1c1a3bd659f3
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-09-18 16:54:37 +0900

    Don't jshint package.json

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -2,7 +2,7 @@
 
 pass=true
 
-files=$(git diff --cached --name-only --diff-filter=ACMR | grep .js | grep -v htdocs/lib)
+files=$(git diff --cached --name-only --diff-filter=ACMR | grep -F ".js" | grep -vE "(htdocs/lib|package.json)")
 if [ "$files" != "" ]; then
 
     # Run JSHint validation before commit
