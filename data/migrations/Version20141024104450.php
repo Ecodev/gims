f4423aeb5578e9eb73476e9cb8a3e2d1df06a268
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-10-26 15:00:20 +0900

    Limit population in existing database #2340

--- data/migrations/Version20141024104450.php
+++ data/migrations/Version20141024104450.php
@@ -10,6 +10,7 @@ use Doctrine\DBAL\Schema\Schema;
  */
 class Version20141024104450 extends AbstractMigration
 {
+
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
@@ -18,10 +19,12 @@ class Version20141024104450 extends AbstractMigration
         $this->addSql("ALTER TABLE questionnaire_usage ADD thematic_filter_id INT DEFAULT NULL");
         $this->addSql("ALTER TABLE questionnaire_usage ADD CONSTRAINT FK_7FB5DD73A7D75635 FOREIGN KEY (thematic_filter_id) REFERENCES filter (id) NOT DEFERRABLE INITIALLY IMMEDIATE");
         $this->addSql("CREATE INDEX IDX_7FB5DD73A7D75635 ON questionnaire_usage (thematic_filter_id)");
+        $this->addSql("DELETE FROM population WHERE year < 1980 OR year > 2020");
     }
 
     public function down(Schema $schema)
     {
         $this->throwIrreversibleMigrationException();
     }
+
 }
