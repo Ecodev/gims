a7d2ea39e342909529844e3d7d21872365d15433
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-10-31 23:18:11 +0900

    Test user is active by default

--- tests/data/sql/user.sql
+++ tests/data/sql/user.sql
@@ -1,1 +1,1 @@
-1	\N	\N	2013-08-22 11:58:37+09	\N	Test user	gims@gims.pro	$2y$14$freAj94ZDP60tRQyVTOyW.2Yh6IMuqJ.a1WrxRgAeWESMCSO62rKe	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N
+1	\N	\N	2013-08-22 11:58:37+09	\N	Test user	gims@gims.pro	$2y$14$freAj94ZDP60tRQyVTOyW.2Yh6IMuqJ.a1WrxRgAeWESMCSO62rKe	1	\N	\N	\N	\N	\N	\N	\N	\N	\N
