ae859248d16455c8880d83e413c62634d6b0a2f8
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-11-06 13:05:19 +0900

    Use fallback for gravatar

--- module/Application/src/Application/Model/User.php
+++ module/Application/src/Application/Model/User.php
@@ -389,7 +389,7 @@ class User extends AbstractModel implements \ZfcUser\Entity\UserInterface, \ZfcR
      */
     public function getGravatar()
     {
-        return 'http://www.gravatar.com/avatar/' . md5(strtolower(trim($this->getEmail())));
+        return 'https://secure.gravatar.com/avatar/' . md5(strtolower(trim($this->getEmail()))) . '?d=identicon';
     }
 
     /**
