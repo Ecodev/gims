f3e4e30be93746f8ecb1cf3d99cb5927ad5cc4bb
Sam Baptista <samuel.baptista@ecodev.ch>
2014-11-21 14:18:47 +0100

    Deploy first level of filter hierarchy #2909

--- htdocs/js/components/filter/filterModalController.js
+++ htdocs/js/components/filter/filterModalController.js
@@ -5,19 +5,11 @@ angular.module('myApp').controller('FilterModalCtrl', function($scope, $modalIns
     var queryParams = params.queryParams ? params.queryParams : {fields: 'color'};
     $scope.expandedNodes = [];
 
-    var expandFirstLevel = function() {
+    $scope.$watch('tree', function() {
         _.forEach($scope.tree, function(filter) {
             $scope.expandedNodes.push(filter);
         });
-    };
-
-    if ($scope.tree) {
-        expandFirstLevel();
-    } else {
-        $scope.$watch('tree', function() {
-            expandFirstLevel();
-        });
-    }
+    });
 
     $scope.treeOptions = {
         dirSelectable: true,
