03ab96860a7a57864e7e2b22f6319c843dc179da
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2014-11-26 17:24:00 +0900

    Prepare for 2015 import #3003

--- bin/import_all_country_data.sh
+++ bin/import_all_country_data.sh
@@ -3,4 +3,4 @@
 echo 'Started. See data/logs/import_all_country.log for progress...'
 > data/logs/all.log
 > data/logs/import_all_country.log
-time find -L data/cache/country_data -type f  \( -name '*_13.xlsm' -o -name '*_13_All_Pop.xlsm' \) ! -name '~$*' -print -exec date \; -exec php htdocs/index.php import jmp {} \; &> data/logs/import_all_country.log
+time find -L data/cache/country_data -type f  \( -name '*_15.xlsm' -o -name '*_15_All_Pop.xlsm' \) ! -name '~$*' -print -exec date \; -exec php htdocs/index.php import jmp {} \; &> data/logs/import_all_country.log
