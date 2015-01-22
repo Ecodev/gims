bd98a04697f9a88f5b3bc49625288b70606506c7
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2015-01-22 15:54:39 +0900

    Officially switch to PHP 5.6

--- tests/ApplicationTest/Controller/AbstractController.php
+++ tests/ApplicationTest/Controller/AbstractController.php
@@ -94,11 +94,6 @@ abstract class AbstractController extends \Zend\Test\PHPUnit\Controller\Abstract
         $prettyActualWithString = json_encode($actualObject, JSON_PRETTY_PRINT);
         $prettyActualWithFloat = \Application\View\Model\NumericJsonModel::stringToNumeric($prettyActualWithString);
 
-        // Very special case for PHP lower than 5.5.12 with wrong JSON prettyfier
-        $prettyActualWithFloat = preg_replace('/\[
-
-\s*\]/', '[]', $prettyActualWithFloat);
-
         // Overwrite log  with given JSON to file for easy comparaison/replacement of existing expected JSON files
         if ($logFile) {
             file_put_contents($logFile, $prettyActualWithFloat);
