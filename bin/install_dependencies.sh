3bc62d27d5869a1348080e1e6a50fc0d3f88e3ae
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2015-02-02 22:35:36 +0900

    Update install process

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -16,7 +16,7 @@ sudo apt-get install -qq software-properties-common # to get next command: add-a
 sudo add-apt-repository --yes ppa:ubuntugis/ubuntugis-unstable
 sudo add-apt-repository --yes ppa:chris-lea/node.js
 sudo apt-get -qq update
-sudo apt-get -qq install postgresql-9.3-postgis-2.1 rubygems nodejs apache2 php5-pgsql php5-cli php5-gd php5-mcrypt php5-intl php5-json
+sudo apt-get -qq install postgresql-9.3-postgis-2.1 nodejs apache2 php5-pgsql php5-cli php5-gd php5-mcrypt php5-intl php5-json php5-redis
 
 echo "Installing php-cs-fixer..."
 sudo wget http://cs.sensiolabs.org/get/php-cs-fixer.phar -O /usr/local/bin/php-cs-fixer
@@ -71,7 +71,7 @@ if [[ "$1" = "configure" ]]; then
     pg_restore --version
     cp config/autoload/local.php.dist config/autoload/local.php
     sudo -u postgres psql -c 'create database gims;' -U postgres
-    createuser --no-superuser --no-createdb --no-createrole gims
-    createuser --no-superuser --no-createdb --no-createrole devgims
-    createuser --no-superuser --no-createdb --no-createrole backup
+    sudo -u postgres createuser --no-superuser --no-createdb --no-createrole gims
+    sudo -u postgres createuser --no-superuser --no-createdb --no-createrole devgims
+    sudo -u postgres createuser --no-superuser --no-createdb --no-createrole backup
 fi
