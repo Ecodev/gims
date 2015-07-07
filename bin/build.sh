a5a97e1e36f34c1b63c32ca95294df1e13cb1b99
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2015-07-07 15:40:22 +0900

    Remove obsolete option

--- bin/build.sh
+++ bin/build.sh
@@ -51,7 +51,7 @@ echo "Updating webdriver..."
 $DEPLOY_USER_SUDO ./node_modules/.bin/webdriver-manager update
 
 echo "Updating all PHP dependencies via composer..."
-composer install --dev --optimize-autoloader
+composer install --optimize-autoloader
 
 echo "Updating database..."
 ./vendor/bin/doctrine-module migrations:migrate --no-interaction
