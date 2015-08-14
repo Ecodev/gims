39318f1626e834eb9e1b109a46527d04e299153f
Adrien Crivelli <adrien.crivelli@ecodev.ch>
2015-08-14 14:26:46 +0900

    Adapt merge script for new headers

--- bin/merge_csv_actual.sh
+++ bin/merge_csv_actual.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-{ tail -q -n +2  *Water.csv ; } | grep  -v 'GLAAS-2013' > _WATER_ACTUAL.csv
-{ tail -q -n +2  *Sanitation.csv ; } | grep  -v 'GLAAS-2013' > _SANITATION_ACTUAL.csv
+{ tail -q -n +4  *Water.csv ; } | grep  -v 'GLAAS-2013' > _WATER_ACTUAL.csv
+{ tail -q -n +4  *Sanitation.csv ; } | grep  -v 'GLAAS-2013' > _SANITATION_ACTUAL.csv
