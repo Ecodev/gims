db7d53fbbf2173189b3647c64fe42faece688dc4
Adrien Crivelli <adrien.crivelli@gmail.com>
2016-09-04 06:35:03 +0200

    Convert Settings/Permissions.plist to configuration/Permissions.php

---
+++ configuration/Permissions.php
@@ -0,0 +1,122 @@
+<?php
+
+return [
+    [
+        'path' => 'data/logs',
+        'permissions' => '0770',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'data/cache',
+        'permissions' => '0770',
+        'recursive' => true,
+    ],
+    [
+        'path' => 'data/session',
+        'permissions' => '0770',
+    ],
+    [
+        'path' => 'vendor/zendframework/zendframework/bin/classmap_generator.php',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'vendor/doctrine/orm/bin/doctrine',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'vendor/doctrine/dbal/bin/doctrine-dbal',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'vendor/doctrine/doctrine-module/bin/doctrine-module',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'vendor/doctrine/orm/bin/doctrine.php',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'vendor/phing/phing/bin/phing',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'vendor/phpunit/phpunit/phpunit',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'vendor/zendframework/zendframework/bin/pluginmap_generator.php',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'vendor/zendframework/zendframework/bin/templatemap_generator.php',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'bin/build.sh',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'bin/import_all_country_data.sh',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'bin/import_test_data.sh',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'bin/install_dependencies.sh',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'bin/live-intro',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'bin/merge_csv_actual.sh',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'bin/merge_csv_expected_country.sh',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'bin/merge_csv_expected_questionnaire.sh',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'bin/pre-commit.sh',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'node_modules/bower/bin/bower',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'node_modules/jscs/bin/jscs',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'node_modules/jshint/bin/jshint',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'node_modules/karma/bin/karma',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'node_modules/ng-annotate/build/es5/ng-annotate',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'node_modules/protractor/bin/protractor',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'node_modules/uglify-js/bin/uglifyjs',
+        'permissions' => '0750',
+    ],
+    [
+        'path' => 'node_modules/protractor/bin/webdriver-manager',
+        'permissions' => '0750',
+    ],
+];
