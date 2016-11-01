4b7cbf651cc277f51233770a500b5af0f9b038e8
Adrien Crivelli <adrien.crivelli@gmail.com>
2016-11-01 15:49:21 +0900

    debug travis

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -4,5 +4,7 @@
 
     which mysql
     mysql --version
-    mysql -e 'CREATE DATABASE gims;'
     mysql -e 'SHOW VARIABLES LIKE "%char%";'
+    mysql -e 'CREATE DATABASE gims CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;'
+    mysql -e 'SHOW VARIABLES LIKE "%char%";'
+    mysql gims -e 'SHOW VARIABLES LIKE "%char%";'
